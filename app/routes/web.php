a916c7578972826488718cfbc2de5c3b5fd70764
igorzeno <igdmstar@mail.ru>
2024-11-06 09:03:39 +0300

    Add merge commit local3

--- app/routes/web.php
+++ app/routes/web.php
@@ -16,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome_t');
+    return view('welcome');
+});
+
+
+Route::get('/', function () {
+    return null;
 });
 
