56756bffb0eae9d7e21e8778782c094ebc2fa52b
igorzeno <igdmstar@mail.ru>
2024-11-07 10:00:01 +0300

    Add HomeController comment

--- app/app/Http/Controllers/HomeController.php
+++ app/app/Http/Controllers/HomeController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class HomeController extends Controller
 {
     /**
@@ -17,12 +15,12 @@ class HomeController extends Controller
 //    }
 
     /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
+     * @return void
      */
+
     public function index()
     {
+        # Comment
         $file = '/var/www/app/Http/Controllers/HomeController.php';
 
         if (is_readable($file)) {
